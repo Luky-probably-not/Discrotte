c89c279f45e457c0a3eebb7e403ccfae262ccf4c
Luky-probably-not <jb.bodusseau@gmail.com>
2026-02-17 15:53:51 +0100

    popup editChannel

--- src/components/channels/PopUpEditChannel.vue
+++ src/components/channels/PopUpEditChannel.vue
@@ -36,56 +36,84 @@ const reloadChannel = () => {
 
 </script>
 <template>
-    <button @click="emit('closeEdition')">X</button>
-    <form>
-        <p>Channel's name</p>
-        <input v-model="currentChannelInfo.name">
-        <p>Channel's picture</p>
-        <img v-bind:src="currentChannelInfo.img"/><br>
-        <input v-model="currentChannelInfo.img"/>
-
-        <p>Themes</p>
-        <label>Primary color</label>
-        <div v-if="currentChannelInfo.theme != undefined">
-            <input v-model="currentChannelInfo.theme.primary_color"/>
-        </div>
-        <div v-else>
-            <input/>
-        </div>
-        <label>Primary color dark</label>
-        <div v-if="currentChannelInfo.theme != undefined">
-            <input v-model="currentChannelInfo.theme.primary_color_dark"/>
-        </div>
-        <div v-else>
-            <input/>
-        </div>
-        <label>Accent color</label>
-        <div v-if="currentChannelInfo.theme != undefined">
-            <input v-model="currentChannelInfo.theme.accent_color"/>
-        </div>
-        <div v-else>
-            <input/>
-        </div>
-        <label>Text color</label>
-        <div v-if="currentChannelInfo.theme != undefined">
-            <input v-model="currentChannelInfo.theme.text_color"/>
-        </div>
-        <div v-else>
-            <input/>
-        </div>
-        <label>Accent text_color</label>
-        <div v-if="currentChannelInfo.theme != undefined">
-            <input v-model="currentChannelInfo.theme.accent_text_color"/>
-        </div>
-        <div v-else>
-            <input/>
-        </div>
-        <button @click="editChanel">Edit channel</button>
-    </form>
+    <section class="popup">
+        <section class="popup-headbar">
+            <p class="name">Edit Channel</p>
+            <button class="close-btn" @click="$emit('closeEdition')">x</button>
+        </section>
+        <form @submit.prevent="editChanel()" class="popup-form">
+            <section class="popup-body">
+                <span class="image-circle">
+                    <img v-bind:src="currentChannelInfo.img"/><br>
+                </span>
+                <section class="inputs-container">
+                    <input v-model="currentChannelInfo.name" type="text"/>
+                    <input v-model="currentChannelInfo.img" type="text"/>
+                    <button type="submit">Update</button>
+                </section>
+            </section>
+        </form>
+    </section>
 </template>
 <style scoped>
-img {
-    width: 200px;
-    height: 150px;
+.popup {
+    width: 1000px;
+    height: 600px;
+    border-radius: 5px;
+    padding: 0 50px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: var(--box-shadow-extern);
+    font-family: var(--text-font-family);
+    background-color: var(--primary-color);
+    border: var(--border-color);
+}
+
+.popup-body {
+  display: flex;
+  gap: 30px;
+  align-items: flex-start;
+}
+
+.popup-form .image-circle {
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+}
+
+.popup-form .image-circle img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.inputs-container {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  flex: 1;
+  margin-top: 10dvh;
+}
+
+.inputs-container input {
+  padding: 8px 10px;
+  border-radius: 5px;
+  border: var(--border-color);
+  font-family: var(--text-font-family);
+  font-size: 1em;
+}
+
+.inputs-container button {
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: var(--border-color);
+  background-color: var(--accent-color);
+  font-weight: var(--boutton-font-weight);
+  cursor: pointer;
+  font-family: var(--text-font-family);
 }
 </style>
